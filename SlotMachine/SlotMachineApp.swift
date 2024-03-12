94c44dd118215888a8694630dc1fea37afa397a2
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-12 21:41:51 +0100

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Maya Ghamloush on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
